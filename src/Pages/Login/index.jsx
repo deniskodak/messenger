f2043b9071a5007b1f2ef32dc9a99e2b769c09e5
Denys Kodak <dkodak@deloitte.ua>
2023-01-05 23:24:46 +0200

    added animation to login page

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 import LoginForm from '../../components/LoginForm'
 import Container from '../../components/Common/Container'
 import FullLogo from '../../components/Common/Logo'
+import FullLetterBubbles from '../../components/Common/Animation/FullLetterBubbles'
 import RegistrationForm from '../../components/RegistrationForm'
 
 const TABS = {
@@ -34,10 +35,11 @@ const Login = () => {
 	const iconClasses = 'h-60 w-full md:w-1/2'
 
 	return (
-		<div>
+		<div className="gradient_animation">
+			<FullLetterBubbles/>
 			<Container>
-				<div className="md:flex flex-wrap lg:h-screen lg:items-center">
-					<div className="p-4 text-center md:flex md:items-center md:w-full lg:flex-col lg:w-1/2 xl:w-1/3 xl:ml-auto">
+				<div className="md:flex flex-wrap lg:h-screen lg:items-center ">
+					<div className=" p-4 text-center md:flex md:items-center md:w-full lg:flex-col lg:w-1/2 xl:w-1/3 xl:ml-auto">
 						<FullLogo
 							iconClasses={iconClasses}
 							labelClasses={labelClasses}
@@ -46,7 +48,7 @@ const Login = () => {
 					<div className="p-4 md:mx-auto md:w-10/12 lg:w-1/2 xl:w-1/3 xl:ml-4">
 						{currentForms[currentTab].form}
 					</div>
-					<div className="text-center md:w-full">
+					<div className="text-center md:w-full pt-2 pb-4">
 						<button
 							onClick={currentForms[currentTab].buttonOnClick}
 							className="underline text-slate-500 text-sm md:text-base">
