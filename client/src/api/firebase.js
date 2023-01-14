33d36447b6b58dad17b66bb034ae1d8164e8bf89
Denys Kodak <dkodak@deloitte.ua>
2023-01-15 01:00:24 +0200

    Messenger: update api keys with end values, add webpack config

--- client/src/api/firebase.js
+++ client/src/api/firebase.js
@@ -1,13 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
+console.log(process.env.REACT_APP_FIREBASE_AUTH)
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_AUTH,
+  apiKey: process.env.REACT_APP_FIREBASE_AUTH,
   authDomain: "quick-messenger-ca6ff.firebaseapp.com",
   projectId: "quick-messenger-ca6ff",
   storageBucket: "quick-messenger-ca6ff.appspot.com",
-  messagingSenderId: process.env.FIREBASE_SENDERID,
-  appId: process.env.FIREBASE_APPID,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APPID,
   measurementId: "G-0BT63N87JG"
 };
 
